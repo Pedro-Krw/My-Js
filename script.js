11e6eeb305ec56b2e06a40660d5f03cdaa3610b4
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-03-08 19:50:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var nama= 'sa pace papua yang paling andalan di biak sana';
+ console.log('ko siapa ! '+nama);
+
+
+
+var pace='';
+
+var pace,papua,mace;
+
+var papua =prompt ('silahkan masukan angka :');
+for (pace=1; pace<=papua; pace++){
+
+	
+ if (pace %2==0){
+
+ 	alert (pace + ' ini adalah bilangan GENAP');
+ }
+ else {
+ 	alert ( pace +' ini adalah bilangan GANJIL');
+ }
+
+}
+	
+
+
+
+
+
+var s='';
+
+var i,j,s;
+for( i=0 ; i<=9; i++){
+	for (j=0; j<=i; j++){
+
+		s+='*';
+	}
+	s +='\n';
+}
+console.log (s)
+
+
