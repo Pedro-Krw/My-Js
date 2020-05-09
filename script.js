bace5b32c96cfb51b0a9119c7be77bfd18a57930
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-05-09 15:09:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,6 @@
-var nama= 'sa napi';
- console.log('ko siapa ! '+nama);
+$(document).ready(function() {
 
-
-
-var pace='';
-
-var pace,papua,mace;
-
-var papua =prompt ('silahkan masukan angka :');
-for (pace=1; pace<=papua; pace++){
-
-	
- if (pace %2==0){
-
- 	alert (pace + ' ini adalah bilangan GENAP');
- }
- else {
- 	alert ( pace +' ini adalah bilangan GANJIL');
- }
-
-}
-	
-
-
-
-
-
-var s='';
-
-var i,j,s;
-for( i=0 ; i<=9; i++){
-	for (j=0; j<=i; j++){
-
-		s+='*';
-	}
-	s +='\n';
-}
-console.log (s)
-
-
+$('#napi').dblclick(function(){
+	$(this).css('background-color','magenta');
+});
+});
