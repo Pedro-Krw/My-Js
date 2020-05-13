61fd31a7e25483edaae85491fd28d64fc998e05f
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-05-13 22:10:24 +0700

    22:10

--- script.js
+++ script.js
@@ -1,6 +1,83 @@
+// event pada saat link di klik
 $(document).ready(function() {
 
-$('#napi').dblclick(function(){
-	$(this).css('background-color','magenta');
+
+	
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+	// pindahkan scroll
+	$('body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1250, 'easeInOutExpo');
+
+	e.preventDefault();
+
 });
+
+// parallax tulisan bergeser
+$(window).on('load', function(){
+	$('.pace').addClass('pace-go');
+	$('.mace').addClass('mace-go')
 });
+// bagian tulisa di jumbotron
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform': 'translate(0px, '+ wScroll / 4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform': 'translate(0px, '+ wScroll / 2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform': 'translate(0px, '+ wScroll / 1.2 +'%)'
+	});
+
+// ini bagian scroll menyala
+  let progress = document.getElementById('progressbar');
+  let totalHeight = document.body.scrollHeight -
+  window.innerHeight;
+  window. onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+  }
+
+  // ini akhirnya dari scroll menyala
+	// portfolio box model
+	if( wScroll > $('.portfolio').offset().top - 100 ) {
+		
+		$('.portfolio .napi').each(function(i) {
+			console.log('yoman');
+			setTimeout(function() {
+				$('.portfolio .napi').eq(i).addClass('muncul');
+			}, 180 * (i+1))
+
+		});
+
+	}
+
+});
+new Typed('#typed',{
+  strings : ['HTML','CSS','BOOTSTRAP','JAVASCRIPT'],
+  typeSpeed : 100,
+  delaySpeed : 100,
+  loop : true
+});
+
+
+$('#slideToggle').click(function(){
+	$('#footer').slideToggle(2000);
+});
+
+
+});
+
+
+
