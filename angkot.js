44d883195d720b3b2e2ac7f3f26bda9f888eac3f
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-05-27 16:42:48 +0700

    16:42

--- angkot.js
+++ angkot.js
@@ -1,6 +1,20 @@
-var angkot = 10 ;
-var napi=1;
+var jumlahAngkot = prompt ('Masukan Angka :')
+var angkotBeroperasi = 6;
+var no5 = 5;
 
-for (napi=1; napi <=angkot; napi++){
-	console.log('Angkot no :' + angkot + 'Beroperasi dengan baik');
+
+for(var noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
+
+	 if (noAngkot <= 6 && noAngkot !==5) {
+	 	console.log('angkot no '+ noAngkot +' Beroperasi dengan baik');
+	 }
+else if(noAngkot === no5){
+     console.log('angkot no '+ no5 + ' sedang Lembur');
+	 }
+else if (noAngkot === 8 ||  noAngkot ===10 ) {
+	console.log('angkot no '+ noAngkot + ' sedang Lembur');
+}
+	 else{
+	 	console.log('angkot no '+ noAngkot + ' Tidak Beroperasi dengan Baik');
+	 }
 }
