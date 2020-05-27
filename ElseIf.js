1187ba70e66b08142b3ab40453d7885be9d59152
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-05-27 15:11:08 +0700

    Add files via upload

---
+++ ElseIf.js
@@ -0,0 +1,11 @@
+var napi = prompt (' silahkan masukan angka yang anda mau :');
+
+if (napi % 2 ===0){
+	alert('Ini adalah bilangan Genap');
+}
+else if (napi %2 === 1){
+	alert('ini adalah bilangan Ganjil');
+}
+else{
+	alert('ehh... topi sa tau ini ko bukan masukan angka jang tipu kami orang PAPUA');
+}
