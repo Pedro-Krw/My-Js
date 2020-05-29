7a96b54a6500830a7e87ff672f349317a3656ae7
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-05-29 17:52:31 +0700

    penembahan beberapa code

--- ElseIf.js
+++ ElseIf.js
@@ -9,3 +9,29 @@ else if (napi %2 === 1){
 else{
 	alert('ehh... topi sa tau ini ko bukan masukan angka jang tipu kami orang PAPUA');
 }
+
+confirm ('Selamat Datang Pada Pola Segitiga')
+  
+var pace = prompt ('Masukan Angka Yang Anda Mau :')
+
+var insos = '';
+
+for (napi=0; napi < pace; napi ++){
+    for(zoro = 0; zoro <= napi; zoro++){
+    	insos +='*';
+    }
+    insos +='\n'
+}
+alert(insos);
+
+var biawak = prompt ('Silahkan Masukan Angka :')
+
+var sapi = '';
+
+for (tante =0; tante < biawak; tante ++){
+	for( anjing=0; anjing <= tante; anjing++){
+		sapi+='+';
+	}
+	sapi+='\n';
+}
+alert(sapi);
