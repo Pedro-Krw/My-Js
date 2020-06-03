f8d16a14f5b400bf50bd516b6a9856eca457d8fc
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-06-03 20:44:26 +0700

    20:44

--- array.js
+++ array.js
@@ -5,3 +5,28 @@ var napi = ['pedro', 'berthus', 'bertho', 2, 4, true, false , 'kucing', ' kam', 
   for (i =0; i < napi.length; i++){
   	alert(napi[i]);
   }
+
+//manipulasi array
+
+var napi = ['pedro', 'berthus', 'bertho', 2, 4, true, false , 'kucing', ' kam', 'semua'];
+ 
+  for (i =0; i < napi.length; i++){
+  	console.log(napi[i]);
+  }  
+
+ var pedro = ['Pedro','Teriyanus','Korwa'];
+   for (i = 0; i < pedro.length; i++){
+   	console.log(pedro.join(' '));
+   }
+
+
+//membuat array dengan mengunakan function
+  function sa (){
+  	var nama = ['Pedro','Teriyanus','Korwa'];
+  	 for (i = 0; i < sa.length; i++){
+
+  	 }
+  	 return nama;
+  }
+
+    console.log(sa().join(' '));
